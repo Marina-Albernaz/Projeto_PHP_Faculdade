ff28eccbb03f892ae56d892a928c67273250bd60
Marina-Albernaz <bubble.commie@gmail.com>
2025-06-02 07:52:20 -0300

    Third Update

--- ProjetoFinalPHP/Viewer/cadastro.php
+++ ProjetoFinalPHP/Viewer/cadastro.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cadastrar-se</title>
+    <title>Cadastrar</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="../CSS/login.css">
